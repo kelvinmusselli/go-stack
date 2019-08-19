09a7bef5a8e2795e57213f791c71a3fd8b56b6a9
Kelvin <kelvin.musselli@iv2.com.br>
2019-08-19 20:24:34 -0300

    finalizado primeiro modulo do omnistack

--- NodeJS/modulo-01/index.js
+++ NodeJS/modulo-01/index.js
@@ -2,17 +2,100 @@ const express = require('express');
 
 const server = express();
 
-server.get('/users/:id', (req, res) => {
+server.use(express.json());
+
+
+const users = [ "Kelvin","Jonas","Tester" ];
+
+
+// criadno uma middleware
+server.use((req, res, next) => {
+    console.time('Request');
+    console.log(`Método: ${req.method}; URL:${req.url }`);
+
+    next();
+    console.timeEnd('Request');
+});
+
+function checkUserExist(req, res, next){
+
+    if(!req.body.name){
+        return res.status(400).json({ erro: "Usuário não encontrado" })
+    }   
+    return next();
+};
+
+function checkPostCreateUser(req, res, next){
+    if(!req.body.name){
+        return res.status(400).json({
+            error:"Parametros incorretos"
+        });
+    }
+    return next();
+};
+
+// aqui ele vai pegar o params na url e vai 
+// rodar todo array ate achar se nao achar ele
+// vai retorno erro e termina ali
+function checkUserInArray (req, res, next) {
+    const user = users[req.params.index];
+    
+    if(!user){
+        return res.status(400).json({
+            error:"Usuário não existe"
+        });
+    }
+    req.user = user;
+
+    return next();
+};
+
+// GET ALL USERS
+server.get(`/users`, (req, res) =>{
+    return res.json(users);
+});
+
+// GET USER BY ID 
+server.get('/users/:index', checkUserInArray, (req, res) => {
 
     // const nome =  req.query.nome;
+    // const id = req.params.id;
+    // const { id } = req.params;
 
-    const id = req.params.id;
- 
-    return res.json({
-        Message: `Buscando o Usuário: ${id}`
-    });
+    // const { index } = req.params;
+    return res.json(req.user);
     // exemplo para enviar 
     // /nome=Kelvin
+});
+
+// POST DE CRIAÇÃO DE USUARIO
+server.post(`/users`, checkPostCreateUser, (req, res) => {
+
+    const { name } = req.body;
+
+    users.push(name);
+     
+    return res.json(users);
+
+});
+// ATUALIZAR USUARIOS BY CODE
+server.put(`/users/:index`, checkUserExist, checkUserInArray, (req, res) => {
+
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+});
+
+// DELETE USER BY CODE
+server.delete(`/users/:index`, checkUserInArray, (req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+    
+    return res.send();
 });
 
 server.listen(3000);
