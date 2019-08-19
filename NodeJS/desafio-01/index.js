1c44ba7e3c2308f7f80f2ff7d692a1d386631d82
Kelvin <kelvin.musselli@iv2.com.br>
2019-08-19 20:38:15 -0300

    REMOVENDO ARQUIVOS INUTEIS DO GITIGNORE

---
+++ NodeJS/desafio-01/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+// ESTA APLICAÇÃO DEVERÁ CRIAR UMA API COM OS SEGUINTES METODOS
+
+// CREATE PROJECTS
+// READ PROJECTS
+// READBYID PROJECTS
+// DELETE PROJECTS
+
+// DEVERÁ CONTER TAMBEM MIDDLEWARES DE TRATAR ERROS
