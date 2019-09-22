97d25a6ce5e903b3390182575232f4fbfe872b44
Kelvin <kelvin.musselli@iv2.com.br>
2019-09-22 11:46:24 -0300

    ok

---
+++ React Native/firstApp/src/index.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+
+import './config/ReactotronConfig';
+
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View/>
+  );
+};
+
+export default App;
